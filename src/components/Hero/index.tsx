f995f0eb2dbbd65082bbf889c7ef6074c9600c47
NARD <glenardrefamonte1@gmail.com>
2024-07-21 22:31:16 +0800

    Updated

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -3,18 +3,20 @@ const Hero = () => {
     <>
       <section className="flex items-center justify-center  mt-16 gap-5 ">
         <section className="w-full  ">
-          <h1 className="  font-bold text-5xl">Hi, I'm Glenard 👋</h1>
-          <p className="text-xl text-[#353637] mt-2">
+          <h1 className="  font-bold text-5xl max-sm:text-3xl">
+            Hi, I'm Glenard 👋
+          </h1>
+          <p className="text-xl text-[#353637] mt-2 max-sm:text-sm">
             Passion-driven and dedicated to creating websites. I am eager to
             apply my extensive knowledge in web development to craft innovative
             and user-friendly online experiences.
           </p>
         </section>
-        <section className="w-[200px]  ">
+        <section className="w-[200px]  max-sm:w-[150px] ">
           <img
             src="/profile-pic.png"
             alt=""
-            className=" w-[130px] h-[130px] rounded-full object-fill"
+            className=" w-[130px] h-[130px] max-sm:w-full max-sm:h-full rounded-full object-fill"
           />
         </section>
       </section>
