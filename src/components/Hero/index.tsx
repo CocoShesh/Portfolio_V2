cc6e31d451f00071b9deb17fa60a0d6644a5d145
NARD <glenardrefamonte1@gmail.com>
2024-07-21 16:37:49 +0800

    Updated

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,5 +1,25 @@
 const Hero = () => {
-  return <div></div>;
+  return (
+    <>
+      <section className="flex items-center justify-center  mt-16 gap-5 ">
+        <section className="w-full  ">
+          <h1 className="  font-bold text-5xl">Hi, I'm Glenard 👋</h1>
+          <p className="text-xl text-[#353637] mt-2">
+            Passion-driven and dedicated to creating websites. I am eager to
+            apply my extensive knowledge in web development to craft innovative
+            and user-friendly online experiences.
+          </p>
+        </section>
+        <section className="w-[200px]  ">
+          <img
+            src="/profile-pic.png"
+            alt=""
+            className=" w-[130px] h-[130px] rounded-full object-fill"
+          />
+        </section>
+      </section>
+    </>
+  );
 };
 
 export default Hero;
