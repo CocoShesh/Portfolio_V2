0657b246d707da17f80bd3ff0b5b97e52a57a339
NARD <glenardrefamonte1@gmail.com>
2024-07-21 23:47:14 +0800

    Updated

--- src/components/WorkExp/data.ts
+++ src/components/WorkExp/data.ts
@@ -4,15 +4,18 @@ interface Experience {
   date: string;
   image: string;
   id: number;
+  responsibilities: string;
 }
 
 export const Work: Experience[] = [
   {
     id: 1,
     company: "Highly Succeed Inc.",
-    position: "Frontend Developer",
+    position: "Frontend Developer Intern",
     date: "January 2024 - April 2024",
     image: "/HSI.jpg",
+    responsibilities:
+      "Transformed UI designs into code. Integrated APIs to ensure seamless functionality. Assisted other interns with their tasks. Ensured responsive design compatibility across various devices and browsers. Participated in daily stand-ups and sprint planning meetings to ensure project milestones were met.",
   },
   {
     id: 2,
@@ -20,5 +23,7 @@ export const Work: Experience[] = [
     position: "Senior High Internship",
     date: "October 2020 - January 2021",
     image: "/city_government_title.jpg",
+    responsibilities:
+      "Responsible for accurately transcribing recorded meetings into text. Ensured the effective documentation of important discussions and information. ",
   },
 ];
