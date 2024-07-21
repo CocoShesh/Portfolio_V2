55a40f7d24a8d8724147db1bb7794dca486a8993
NARD <glenardrefamonte1@gmail.com>
2024-07-21 23:47:02 +0800

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import About from "./components/About";
+import Education from "./components/Education";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import WorkExp from "./components/WorkExp";
@@ -8,10 +9,11 @@ function App() {
   return (
     <>
       <main className="w-full h-screen     flex items-center justify-center">
-        <section className="w-[650px] max-sm:px-5 h-full flex flex-col">
+        <section className="w-[650px] max-md:px-5 h-full flex flex-col">
           <Hero />
           <About />
           <WorkExp />
+          <Education />
         </section>
         <Navbar />
       </main>
