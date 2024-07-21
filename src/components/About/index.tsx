bebd5c7f6cff2402694e4cb1564fbaa4a713f84c
NARD <glenardrefamonte1@gmail.com>
2024-07-21 22:31:12 +0800

    Updated

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,22 +1,52 @@
 const About = () => {
   return (
     <>
-      <h3> About</h3>
-      <p>
-        {" "}
-        As a graduate of a Bachelor of Science in Information Technology (BSIT),
-        I am deeply passionate about web development and am currently seeking a
-        job opportunity to apply and further develop my skills in a professional
-        environment. With a solid foundation nurtured through academic
-        coursework and self-directed learning from diverse resources, such as
-        YouTube videos, tutorials, and documentation, I am equipped with a
-        versatile toolkit to tackle challenges in the field. Driven by curiosity
-        and a hunger for hands-on experience, I am enthusiastic about applying
-        my knowledge to real-world projects and collaborating with experienced
-        professionals. My goal is not only to enhance my technical proficiency
-        but also to make a tangible impact within the organization, leveraging
-        creativity and problem-solving skills to deliver innovative solutions.
-      </p>
+      <section className="mt-10">
+        <h3 className="font-bold text-2xl mb-1"> About</h3>
+        <p className="text-[#8a8a8a]">
+          As a graduate of a {""}
+          <span className="text-[#424853] font-semibold underline">
+            Bachelor of Science in Information Technology (BSIT),
+          </span>{" "}
+          {""}I am deeply passionate about
+          <span className="text-[#424853] font-semibold underline">
+            {" "}
+            web development{" "}
+          </span>
+          and am currently seeking a job opportunity to apply and further
+          develop my skills in a professional environment. With a solid
+          foundation nurtured through academic coursework and self-directed
+          learning from diverse resources, such as
+          <span className="text-[#424853] font-semibold underline">
+            {" "}
+            YouTube videos,
+          </span>{" "}
+          <span className="text-[#424853] font-semibold underline">
+            tutorials
+          </span>
+          , and
+          <span className="text-[#424853] font-semibold underline">
+            {" "}
+            documentation,
+          </span>{" "}
+          I am equipped with a versatile toolkit to tackle challenges in the
+          field.
+        </p>
+        <p className="text-[#8a8a8a] mt-3">
+          Driven by curiosity and a hunger for{" "}
+          <span className="text-[#424853] font-semibold underline">
+            hands-on experience,
+          </span>{" "}
+          I am enthusiastic about applying my knowledge to{" "}
+          <span className="text-[#424853] font-semibold underline">
+            real-world projects
+          </span>{" "}
+          and collaborating with experienced professionals. My goal is not only
+          to enhance my technical proficiency but also to make a tangible impact
+          within the organization, leveraging creativity and problem-solving
+          skills to deliver innovative solutions.
+        </p>
+      </section>
     </>
   );
 };
