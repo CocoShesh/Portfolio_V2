69135ce5bc69c41cdcb14046b35f5a90dd93639b
NARD <glenardrefamonte1@gmail.com>
2024-07-21 23:59:55 +0800

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import WorkExp from "./components/WorkExp";
 function App() {
   return (
     <>
-      <main className="w-full h-screen     flex items-center justify-center">
+      <main className="w-full h-screen select-none    flex items-center justify-center">
         <section className="w-[650px] max-md:px-5 h-full flex flex-col">
           <Hero />
           <About />
