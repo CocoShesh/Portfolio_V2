d0713f87f328732e555479132f21d6a124665350
NARD <glenardrefamonte1@gmail.com>
2024-07-21 16:37:40 +0800

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <main className="relative">
+      <main className="w-full h-screen    flex items-center justify-center">
+        <section className="w-[650px]  h-full flex flex-col">
+          <Hero />
+        </section>
         <Navbar />
       </main>
     </>
