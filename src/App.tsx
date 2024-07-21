30b0aa39da8eaaae847b8cb9f0f5e6dadb64da0b
NARD <glenardrefamonte1@gmail.com>
2024-07-21 22:31:04 +0800

    Import Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import "./App.css";
+import About from "./components/About";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import WorkExp from "./components/WorkExp";
 
 function App() {
   return (
     <>
-      <main className="w-full h-screen    flex items-center justify-center">
-        <section className="w-[650px]  h-full flex flex-col">
+      <main className="w-full h-screen     flex items-center justify-center">
+        <section className="w-[650px] max-sm:px-5 h-full flex flex-col">
           <Hero />
+          <About />
+          <WorkExp />
         </section>
         <Navbar />
       </main>
