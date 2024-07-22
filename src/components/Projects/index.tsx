75ba58b5197ed8b183213ef4d76ab8eb5f0c005e
NARD <glenardrefamonte1@gmail.com>
2024-07-22 12:46:14 +0800

    Updated

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -3,7 +3,10 @@ import { projects } from "./data";
 const Projects = () => {
   return (
     <>
-      <section className="mt-16  text-center pb-32 text-sm max-sm:text-xs">
+      <section
+        id="projects"
+        className="mt-16  text-center pb-32 text-sm max-sm:text-xs"
+      >
         <span className=" bg-black text-white px-3 py-1 rounded-md  mb-1">
           My Projects{" "}
         </span>
@@ -25,11 +28,11 @@ const Projects = () => {
               <section className="px-3 py-2">
                 <h1 className=" font-bold "> {list.title} </h1>
                 <p className="mt-2 text-xs"> {list.descriptions}</p>
-                <section className="flex flex-wrap mt-2">
+                <section className="flex flex-wrap mt-2 gap-2">
                   {list.techStack.map(tech => (
                     <span
                       key={tech}
-                      className="bg-[#f7f7f7]  font-medium px-2 py-1 text-xs rounded-md mr-2"
+                      className="bg-[#f7f7f7]  font-medium px-2 py-1 text-xs rounded-md"
                     >
                       {tech}
                     </span>
@@ -39,15 +42,15 @@ const Projects = () => {
                   <a
                     href={list.liveLink}
                     target="_blank"
-                    className="px-2 py-1 text-xs flex gap-1 items-center rounded-md bg-black text-white "
+                    className="px-2 py-1 text-xs flex gap-1 items-center hover:bg-[#454545] rounded-md bg-black text-white "
                   >
-                    <img src="./icon/globe.svg" alt="" className="h-3 " />{" "}
+                    <img src="./icon/globe.svg" alt="" className="h-3" />{" "}
                     Website
                   </a>
                   <a
                     href={list.githubLink}
                     target="_blank"
-                    className="px-2 py-1 text-xs flex gap-1 items-center rounded-md bg-black text-white "
+                    className="px-2 py-1 text-xs flex gap-1 items-center rounded-md hover:bg-[#454545]  bg-black text-white "
                   >
                     <img
                       src="./icon/github-white.svg"
